1e02fcb4bcc09ededd760223d3f070d731a800f0
Thomas <thomas@craftacademy.se>
2021-11-02 21:08:34 +0100

    adds booksController

--- app.js
+++ app.js
@@ -1,32 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 
-const pool = require("./config");
+const booksController = require("./booksController");
 
 const app = express();
-
-const getBooks = (request, response) => {
-  pool.query("SELECT * FROM books;", (error, results) => {
-    response.status(200).json({ books: results.rows });
-  });
-};
-
-const createBook = (request, response) => {
-  const { author, title } = request.body;
-  pool.query(
-    "INSERT INTO books (author, title) VALUES ($1, $2)",
-    [author, title],
-    () => {
-      response.status(201).json({ message: "success" });
-    }
-  );
-};
 
 app
   .use(express.json())
   .use(express.urlencoded({ extended: true }))
   .use(cors({ credentials: true, origin: ["http://localhost:3474"] }));
 
-app.route("/").get(getBooks).post(createBook);
+app
+  .route("/")
+  .get(booksController.index)
+  .post(booksController.create)
+  .put(booksController.update)
+  .delete(booksController.delete);
 
 app.listen(3001, () => console.log("Server is listening on port 3001"));
