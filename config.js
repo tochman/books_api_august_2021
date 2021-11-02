514917ca162ae48325bf9d18b600d551faf16049
Thomas <thomas@craftacademy.se>
2021-11-02 15:05:55 +0100

    sets up db connection adds basic project structure

---
+++ config.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+const { Pool } = require("pg");
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_POST}/${process.env.DB_DATABASE}`
+
+const pool = new Pool({
+  connectionString: connectionString,
+  ssl: false
+})
+
+module.exports = pool
